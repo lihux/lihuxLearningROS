dd1dcc400afc532eda80a56136691bff0b0521dc
lihui <lihui.zl@163.com>
2014-05-04 22:53:17 +0800

    lihux ros init

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/parallels/ros/ros_workspace/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/parallels/ros/ros_workspace/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/parallels/ros/ros_workspace/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/parallels/ros/ros_workspace/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/parallels/ros/ros_workspace/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/parallels/ros/ros_workspace/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/parallels/ros/ros_workspace/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/parallels/ros/ros_workspace/build"
+export PYTHONPATH="/home/parallels/ros/ros_workspace/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/parallels/ros/ros_workspace/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/parallels/ros/ros_workspace/src:$ROS_PACKAGE_PATH"
